eee3d80b2eff621eff4518691dcdf6a588893823
Kristofer Lund <kristofer@fmckl.se>
2025-12-26 16:29:56 +0100

    Use release versions instead of local

--- vite.config.ts
+++ vite.config.ts
@@ -31,7 +31,6 @@ export default defineConfig({
       "/api": {
         target: `http://127.0.0.1:4943`,
         changeOrigin: true,
-        rewrite: (path) => path.replace(/^\/api/, "/api"),
       },
     },
   },
